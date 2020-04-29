fc7590054eb584035c2b71b25d682f5e1ffe0b5c
QT-Zhu <zhuqingtian971114@126.com>
2020-04-29 19:09:33 +0800

    Update AerialTransforms.py

--- custom_transforms/AerialTransforms.py
+++ custom_transforms/AerialTransforms.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 class TrainAug(object):
     def __init__(self,crop_size):
         #self.crop_trans = transforms.RandomCrop(crop_size)
-        self.crop_trans = transforms.ZQTRandomCrop(crop_size)
+        #self.crop_trans = transforms.ZQTRandomCrop(crop_size)
         self.hflip_trans = transforms.RandomHorizontalFlip()
         self.vflip_trans = transforms.RandomVerticalFlip()
         #self.rotate_trans = transforms.RandomRotation(degrees=[0,90,180,270])
@@ -15,7 +15,7 @@ class TrainAug(object):
         self.normalize_trans = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
     
     def __call__(self,img,gt): #img and gt are PIL objects
-        _, img ,gt = self.crop_trans(img,gt) #coordination is useless for training
+        #_, img ,gt = self.crop_trans(img,gt) #coordination is useless for training
         
         img, gt = self.hflip_trans(img,gt)
         img, gt = self.vflip_trans(img,gt)
@@ -30,12 +30,12 @@ class TrainAug(object):
 #deprecated
 class EvalAug(object):
     def __init__(self,crop_size):
-        self.crop_trans = transforms.RandomCrop(crop_size)
+        #self.crop_trans = transforms.RandomCrop(crop_size)
         self.tensor_trans = transforms.ToTensor()
         self.normalize_trans = transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
     
     def __call__(self,img,gt):
-        _, img, gt = self.crop_trans(img,gt)
+        #_, img, gt = self.crop_trans(img,gt)
         img, gt = self.tensor_trans(img,gt)
         img_tensor = self.normalize_trans(img)
         gt = gt.squeeze()
